c5fa439d5a6204a7e75f241da1a0121869ee4449
Mike Lazer-Walker <michael@lazerwalker.com>
2018-05-23 12:31:32 +0200

    Render a billboard sprite

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import React from 'react';
 class App extends React.Component {
   render () {
     return (
-      <Scene>
-        <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
-        <Entity text={{value: 'Hello, WebVR!'}} position={{x:0, y: 1, z: -4}} color={'blue'}/>
-      </Scene>
+      <div>
+        <img id='wol' src="WoL.png"/>
+        <Scene>
+          <Entity geometry={{primitive: 'plane'}} material={{src: '#wol'}} position={{x: 0, y: 0, z: -5}} width={1} height={1}/>
+        </Scene>
+      </div>
     );
   }
 }
